f715d8b3d78497d2605b971b2797a94bb374b28e
Allan MacGregor <amacgregor@allanmacgregor.com>
2014-01-11 11:56:16 -0500

    Create user table with google oauth fields

---
+++ app/database/migrations/2014_01_11_165412_create_user_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateUserTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('user', function(Blueprint $table) {
+			$table->increments('id');
+            $table->string('email');
+            $table->string('firstname');
+			$table->string('lastname');
+			$table->string('goggle_accesstoken');
+			$table->boolean('is_admin');
+            $table->softDeletes();
+            $table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('user');
+	}
+
+}
