6c2a3c17c6b3db939c6b75289dec73438195dfa1
Allan MacGregor <amacgregor@allanmacgregor.com>
2014-01-11 22:50:49 -0500

    Update for project list display

--- app/views/projects/index.blade.php
+++ app/views/projects/index.blade.php
@@ -3,11 +3,32 @@
 @section('main')
 
 <h1>MageHack Projects:</h1>
+
+
 <div class="projects-container" ng-controller="ProjectsController">
-    <div class="project" ng-repeat="project in projects">
-        <h3>@{{ project.title }}</h3>
-        <p>@{{ project.description }}</p>
+    <div class="panel panel-default project" ng-repeat="project in projects">
+        <div class="panel-heading">
+            <h3 class="panel-title">@{{ project.title }}</h3>
+        </div>
+        <div class="panel-body">
+            <p>@{{ project.description }}</p>
+        </div>
     </div>
 </div>
+
+
+@if(Auth::check())
+<h1>Submit Your Own Project:</h1>
+
+{{ Form::open(array('action' => 'ProjectController@create')) }}
+{{ Form::label('title', 'Title:'); }}
+{{ Form::text('title','', array('class' => 'form-control')); }}
+{{ Form::label('description', 'Description:'); }}
+{{ Form::textarea('description','', array('class' => 'form-control')); }}
+{{ Form::submit('Submit'); }}
+{{ Form::close() }}
+
+@endif
+
 
 @stop
