9e879dd38f25599a1f829b67a20e4b40098e3c27
Allan MacGregor <amacgregor@allanmacgregor.com>
2014-01-12 14:11:05 -0500

    Update for Project table seeder

--- app/database/seeds/ProjectsTableSeeder.php
+++ app/database/seeds/ProjectsTableSeeder.php
@@ -9,8 +9,8 @@ class ProjectsTableSeeder extends Seeder {
 
 		$projects = array(
             array(
-                'title' => 'Test Project',
-                'description' => 'Example project description for a project idea',
+                'title' => 'Implement Module for prediction.io',
+                'description' => 'There is a service called http://prediction.io/ which targets on predicting user behaviors. Could be very interesting for recommendation backend',
                 'user_id' => 1,
                 'hangout_url' => '',
                 'created_at' => '2014-01-12 00:00:00',
@@ -18,8 +18,17 @@ class ProjectsTableSeeder extends Seeder {
 
             ),
             array(
-                'title' => 'Example Project',
-                'description' => 'Example project description for a project idea',
+                'title' => 'Real Big Sample Data',
+                'description' => 'Mainly for performanc research I would like to have a real real big dataset which covers all imaginably performance edge cases. some of them are: * many many categories whith more then 5 levels of depth. lets say 1k+ categories * configurable products with several options ( in the end 1 configurable = 10k+ simples {20×10×4×6×3}) * multiple websites/stores/storeviews ( should end in 1k storeviews ) Its ok(and suggested) if it ends in a script, which randomly creates this into files readable by an Importer, as this would speed up the generation and the importer can care in the end of optimize the saving to magento.',
+                'user_id' => 1,
+                'hangout_url' => '',
+                'created_at' => '2014-01-12 00:00:00',
+                'updated_at' => '2014-01-12 00:00:00',
+
+            ),
+            array(
+                'title' => 'Shipping rates grid in Magento',
+                'description' => 'I think it would be nice to have something like this https://github.com/thebod/Thebod_Shippingrates in magento, this modfication of Magento will allow nice and easy managment of shipping rates out of magento box.',
                 'user_id' => 1,
                 'hangout_url' => '',
                 'created_at' => '2014-01-12 00:00:00',
