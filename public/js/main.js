241afd2accea52ea3a6f6216b26c35d62cc83acd
Allan MacGregor <amacgregor@allanmacgregor.com>
2014-01-11 22:51:12 -0500

    Main.js update for mock data

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 function ProjectsController($scope){
     $scope.projects = [
-        { title: 'Project Number one', description: 'Something important about the project'},
-        { title: 'Project Number two', description: 'Something important about the project'}
+        { title: 'Project Number one', description: 'Something important about the project', user_id: 1},
+        { title: 'Project Number two', description: 'Something important about the project', user_id: 1}
     ];
 }
