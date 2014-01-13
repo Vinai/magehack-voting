f2124f503fa2599e7ff424c7732cb895eaa0f90c
Vinai Kopp <vinai@netzarbeiter.com>
2014-01-13 18:11:28 +0100

    Added tests for user permissions

--- tests/js/service-test.js
+++ tests/js/service-test.js
@@ -10,16 +10,10 @@ describe('The mage hackathon service', function () {
     beforeEach(inject(function (Service) {
         service = Service
     }));
-
-    // public
+    
     it('should have a authenticateUser method', function () {
         expect(service.authenticateUser).toBeDefined();
         expect(typeof service.authenticateUser).toBe('function');
-    });
-    
-    it('should have a getUserInfo resource', function () {
-        expect(service.getUserInfo).toBeDefined();
-        expect(typeof service.getUserInfo).toBe('function');
     });
     
     it('should have a getProjects resource', function () {
@@ -27,10 +21,19 @@ describe('The mage hackathon service', function () {
         expect(typeof service.getProjects).toBe('function');
     });
 
-    // authorized users
     it('should have a createProject resource', function () {
         expect(service.createProject).toBeDefined();
         expect(typeof service.createProject).toBe('function');
+    });
+
+    it('should have a updateProject resource', function () {
+        expect(service.updateProject).toBeDefined();
+        expect(typeof service.updateProject).toBe('function');
+    });
+
+    it('should have a deleteProject resource', function () {
+        expect(service.deleteProject).toBeDefined();
+        expect(typeof service.deleteProject).toBe('function');
     });
     
     it('should have a createVote resource', function () {
@@ -41,16 +44,35 @@ describe('The mage hackathon service', function () {
         expect(service.deleteVote).toBeDefined();
         expect(typeof service.deleteVote).toBe('function');
     });
-
-    // creator user + admin
-    it('should have a updateProject resource', function () {
-        expect(service.updateProject).toBeDefined();
-        expect(typeof service.updateProject).toBe('function');
-    });
-
-    // admin (TBD: also creators?)
-    it('should have a deleteProject resource', function () {
-        expect(service.deleteProject).toBeDefined();
-        expect(typeof service.deleteProject).toBe('function');
-    });
+    
+    if ('should allow non-authorized users to call authenticateUser', function() {});
+    if ('should allow authorized users to call createProject', function() {});
+    if ('should allow admins to call createProject', function() {});
+    
+    if ('should allow non-authorized users to call getProjects', function() {});
+    if ('should allow authorized users to call createProject', function() {});
+    if ('should allow admins to call createProject', function() {});
+    
+    if ('should not allow non-authorized users to call createProject', function() {});
+    if ('should allow authorized users to call createProject', function() {});
+    if ('should allow admins to call createProject', function() {});
+    
+    if ('should not allow non-authorized users to call updateProject', function() {});
+    if ('should not allow authorized users that are not the creator to call updateProject', function() {});
+    if ('should allow authorized users that are the creator to call updateProject', function() {});
+    if ('should allow admins to call updateProject', function() {});
+    
+    if ('should not allow non-authorized users to call deleteProject', function() {});
+    if ('should not allow authorized users that are not the creator to call deleteProject', function() {});
+    if ('should not allow authorized users that are the creator to call deleteProject', function() {});
+    if ('should allow admins to call deleteProject', function() {});
+    
+    if ('should not allow non-authorized users to call createVote', function() {});
+    if ('should allow authorized users to call createVote', function() {});
+    if ('should allow admins to call createVote', function() {});
+    
+    if ('should not allow non-authorized users to call deleteVote', function() {});
+    if ('should allow authorized users to delete their own votes by calling deleteVote', function() {});
+    if ('should not allow authorized users to delete other users votes by calling deleteVote', function() {});
+    if ('should allow admins to call deleteVote', function() {});
 });
